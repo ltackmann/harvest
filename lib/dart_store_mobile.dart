0409027d6ec87e0043969d9534e43e82706cde7e
Lars Tackmann <ltackmann@gmail.com>
2013-01-05 19:07:55 +0100

    fix copyright

--- lib/dart_store_mobile.dart
+++ lib/dart_store_mobile.dart
@@ -1,4 +1,4 @@
-// Copyright (c) 2012 Solvr, Inc. All rights reserved.
+// Copyright (c) 2013 Solvr, Inc. All rights reserved.
 //
 // This open source software is governed by the license terms 
 // specified in the LICENSE file
